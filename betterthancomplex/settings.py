1d08b87bf0da15b9904d865cfd7c4a4fdae5d2c0
Sachin Singh <sachin_rcz@yahoo.co.in>
2017-11-27 22:23:50 +0530

    test

--- betterthancomplex/settings.py
+++ betterthancomplex/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'assets'),
+# ]
