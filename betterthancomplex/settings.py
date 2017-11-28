d1e2d6609fc9c533b4302edd01cb07b659f7a626
Sachin Singh <sachin_rcz@yahoo.co.in>
2017-11-28 22:04:54 +0530

    test

--- betterthancomplex/settings.py
+++ betterthancomplex/settings.py
@@ -47,12 +47,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'betterthancomplex.urls'
@@ -128,5 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'assets'),
+#     os.path.join(BASE_DIR, 'static'),
 # ]
